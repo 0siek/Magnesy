b7464b09c8b494f8e412b0689d1e33fc66327552
0siek <patrykkajda89@gmail.com>
2024-06-19 11:26:55 +0200

    tak

--- sendmail.js
+++ sendmail.js
@@ -1,24 +1,49 @@
-const axios = require("axios");
+document
+  .getElementById("discordForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Zatrzymanie domyślnego działania formularza
 
-module.exports = async (req, res) => {
-  const { name, email, subject, message } = req.body;
+    const name = document.getElementById("imie").value;
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("temat").value;
+    const message = document.getElementById("message").value;
 
-  const data = {
-    from: "your-email@example.com",
-    to: email,
-    subject: `${name}: ${subject}`,
-    text: message,
-  };
+    const request = new XMLHttpRequest();
+    request.open(
+      "POST",
+      "https://discord.com/api/webhooks/1244811364699209808/2bTKekdmCTsHfqdc3qkrdmr-zzA4wBvcSW6DLFZBmCa9ImNsQx6T-FKH5VIU2LnXIXVm"
+    );
 
-  try {
-    await axios.post("https://api.resend.io/v1/mail", data, {
-      headers: {
-        re_6C5tXd9t_KvJ1rP1DzJYFNc46KYAqXaWb: process.env.RESEND_API_KEY,
-      },
+    request.setRequestHeader("Content-type", "application/json");
+
+    const now = new Date();
+    const date = now.toLocaleDateString("pl-PL");
+    const time = now.toLocaleTimeString("pl-PL", {
+      hour: "2-digit",
+      minute: "2-digit",
     });
-    res.status(200).send("Wiadomość wysłana pomyślnie");
-  } catch (error) {
-    console.log("Błąd", error);
-    res.status(400).send("Wystąpił problem z wysłaniem wiadomości");
-  }
-};
+
+    const params = {
+      username: "Captain Hook",
+      avatar_url: "",
+      content: `<@545287551804506113> Ping!`,
+      embeds: [
+        {
+          author: {
+            name: name,
+          },
+          title: subject,
+          description: `**Wiadomość:**\n${message}\n\n**Email:**\n${email}`,
+          footer: {
+            text: `Wysłano • ${date} ${time}`,
+          },
+          color: 3447003, // Hex color code for embed's left border (e.g., blue)
+        },
+      ],
+    };
+
+    request.send(JSON.stringify(params));
+
+    // Czyszczenie formularza
+    document.getElementById("discordForm").reset();
+  });

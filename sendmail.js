85569115b0e933b3865a143a58091fe71b4e9e9f
0siek <patrykkajda89@gmail.com>
2024-06-19 12:44:24 +0200

    v1.2

--- sendmail.js
+++ sendmail.js
@@ -3,10 +3,41 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault(); // Zatrzymanie domyślnego działania formularza
 
+    const tel = document.getElementById("tel").value;
     const name = document.getElementById("imie").value;
     const email = document.getElementById("email").value;
     const subject = document.getElementById("temat").value;
     const message = document.getElementById("message").value;
+    const errorMessages = document.getElementById("errorMessages");
+
+    // Czyszczenie poprzednich błędów
+    errorMessages.innerHTML = "";
+
+    // Walidacja danych
+    const telPattern = /^[0-9]{9}$/; // Prosty wzór dla polskiego numeru telefonu (9 cyfr)
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Podstawowy wzór dla adresu email
+
+    if (!name) {
+      errorMessages.innerHTML = "<p>Proszę wpisać imię.</p>";
+      return;
+    }
+    if (!emailPattern.test(email)) {
+      errorMessages.innerHTML = "<p>Proszę wpisać poprawny adres email.</p>";
+      return;
+    }
+    if (!telPattern.test(tel)) {
+      errorMessages.innerHTML =
+        "<p>Proszę wpisać poprawny numer telefonu (9 cyfr).</p>";
+      return;
+    }
+    if (!subject) {
+      errorMessages.innerHTML = "<p>Proszę wpisać temat.</p>";
+      return;
+    }
+    if (!message) {
+      errorMessages.innerHTML = "<p>Proszę wpisać wiadomość.</p>";
+      return;
+    }
 
     const request = new XMLHttpRequest();
     request.open(
@@ -33,7 +64,7 @@ document
             name: name,
           },
           title: subject,
-          description: `**Wiadomość:**\n${message}\n\n**Email:**\n${email}`,
+          description: `**Wiadomość:**\n${message}\n\n**Email:**\n${email}\n\n**Telefon:**\n${tel}`,
           footer: {
             text: `Wysłano • ${date} ${time}`,
           },
